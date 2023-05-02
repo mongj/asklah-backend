2051667a3e6203cabd3f7febc93af7bd2cf24346
mongj <evan.zhangmingjun@gmail.com>
2023-05-02 14:45:36 +0800

    build: update config

---
+++ config.example.py
@@ -0,0 +1,12 @@
+# Pinecone
+PINECONE_API_KEY = ""
+PINECONE_ENV = ""
+PINECONE_INDEX_NAME = ""
+# Currently using the all-MiniLM-L6-v2 model for testing (https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2)
+PINECONE_INDEX_DIMENSIONS = 384
+
+# Testing
+TESTING_FOLDER = ""
+
+# Visualisation
+NOMIC_API_KEY = ""
